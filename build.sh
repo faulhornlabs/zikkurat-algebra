f484ee9eb9057a317610260089115ff75f90aa59
Balazs Komuves <bkomuves@gmail.com>
2023-07-09 10:05:16 +0200

    commit add compilation flags to enable/disable particular curves

--- build.sh
+++ build.sh
@@ -17,21 +17,25 @@ echo "===================="
 #echo "current directory = `pwd`"
 cabal install --disable-documentation || error_exit "building the codegen failed"
 
+FLAGS=""
+
 cd ${ROOT}/lib
 echo "===================="
 echo "building the library"
 echo "===================="
 #echo "current directory = `pwd`"
-cabal clean     || error_exit "cleaning the lib failed"
-cabal configure || error_exit "configuring the lib failed"
-cabal build     || error_exit "building the lib failed"
-cabal haddock   || error_exit "haddocks for the lib failed"
-cabal install   || error_exit "installing the lib failed"
+cabal clean              || error_exit "cleaning the lib failed"
+cabal configure ${FLAGS} || error_exit "configuring the lib failed"
+cabal build              || error_exit "building the lib failed"
+cabal haddock            || error_exit "haddocks for the lib failed"
+cabal install            || error_exit "installing the lib failed"
 
 cd ${ROOT}/test
 echo "======================"
 echo "building the testsuite"
 echo "======================"
+cabal clean
+cabal configure
 cabal build || error_exit "building the test failed"
 
 echo "OK!"
